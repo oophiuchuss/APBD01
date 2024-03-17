3149489f7b6993230ef01e8f8b98ba8834ff263d
Oleksandr Smirnov <116755622+oophiuchuss@users.noreply.github.com>
2024-03-17 21:07:19 +0100

    Revert "Method CalculateAverage created."
    This reverts commit ec5c23ca1fdcd3e9e0c1bdfec2362f86692614b5.

--- APBD01/Program.cs
+++ APBD01/Program.cs
@@ -1,17 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-List<int> tmp = new List<int>();
-tmp.Add(1);
-tmp.Add(2);
-tmp.Add(3);
-tmp.Add(4);
-tmp.Add(1);
-tmp.Add(-10);
-
-Console.WriteLine(CalculateAverage(tmp));
-
-
-
-double CalculateAverage(List<int> ToCalcualte)
-{
-    return ToCalcualte.Average(); ;
-}
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
