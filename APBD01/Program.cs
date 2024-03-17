ec5c23ca1fdcd3e9e0c1bdfec2362f86692614b5
Oleksandr Smirnov <116755622+oophiuchuss@users.noreply.github.com>
2024-03-17 21:06:11 +0100

    Method CalculateAverage created.

--- APBD01/Program.cs
+++ APBD01/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+List<int> tmp = new List<int>();
+tmp.Add(1);
+tmp.Add(2);
+tmp.Add(3);
+tmp.Add(4);
+tmp.Add(1);
+tmp.Add(-10);
+
+Console.WriteLine(CalculateAverage(tmp));
+
+
+
+double CalculateAverage(List<int> ToCalcualte)
+{
+    return ToCalcualte.Average(); ;
+}
