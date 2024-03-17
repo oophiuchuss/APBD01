2a0a8e1ef65dc8cc595d36fadb53c84fd424fc01
Oleksandr Smirnov <116755622+oophiuchuss@users.noreply.github.com>
2024-03-17 21:15:46 +0100

    Created CalculateAverage

--- APBD01/Program.cs
+++ APBD01/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
+
+double CalculateAverage(List<int> ToCalcualte)
+{
+    return ToCalcualte.Average(); ;
+}
